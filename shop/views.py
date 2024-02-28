e4c22d42f27774f3daad1a5c62c60cc7a5ec5139
KaTicb <trick1204ed@mail.ru>
2024-02-28 16:07:29 +0300

    migrate

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
-# Create your views here.
+
+def index(request):
+    courses = models.Course.objects.all()
+    return HttpResponse(''.join([str(course) + '<br>' for course in courses]))
